d61084dfb0655bd6016dbdfeabdd8a797bcedd35
akuiiki <43848105+Myudi422@users.noreply.github.com>
2022-03-21 17:13:10 +0700

    tes

--- plugins/commands.py
+++ plugins/commands.py
@@ -29,3 +29,108 @@ def _cleandir(bot, update):
 def _tes(bot, update):
     update.reply_text
 
+@Client.on_message(filters.private & filters.command('browse'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('anime'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('menu'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('manga'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('character'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('quote'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('anilist'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('auth'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('schedule'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('airing'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('manga_s'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('getgenres'))
+def _tes(bot, update):
+    update.reply_text    
+
+@Client.on_message(filters.private & filters.command('gettags'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('watch'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('wallpaper'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('waifu'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('reverse'))
+def _tes(bot, update):
+    update.reply_text     
+
+@Client.on_message(filters.private & filters.command('favourites'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('me'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('flex'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('user'))
+def _tes(bot, update):
+    update.reply_text              
+@Client.on_message(filters.private & filters.command('ping'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('fillers'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('logout'))
+def _tes(bot, update):
+    update.reply_text    
+@Client.on_message(filters.private & filters.command('top'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('top'))
+def _tes(bot, update):
+    update.reply_text
+
+@Client.on_message(filters.private & filters.command('top'))
+def _tes(bot, update):
+    update.reply_text                     
